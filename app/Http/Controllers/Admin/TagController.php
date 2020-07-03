fcae97b47dae4fff0819be19c73776d831955503
mishajib <mishajib222@gmail.com>
2020-07-03 21:18:06 +0600

    post preview added into admin panel

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -28,7 +28,7 @@ class TagController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index()
     {
